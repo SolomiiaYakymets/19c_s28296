d75b81e83e39c055133db9f23a200476469509b0
SolomiiaYakymets <s28296@pjwstk.edu.pl>
2024-03-19 16:50:22 +0100

    task 4 math library is utilized to calculate the square roots of numbers

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import math
+
+
 class SquareGenerator:
     def e_squares(self, start, end):
         squares = [number * number for number in range(start, end)]
         return squares
+
+
+starting_num = int(input("Enter the starting number: "))
+ending_num = int(input("Enter the ending number: "))
+square_generator = SquareGenerator()
+square_root = [math.sqrt(number) for number in square_generator.e_squares(starting_num, ending_num)]
