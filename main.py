f3014fae94d2a509881f6d99d242156248cc43f5
SolomiiaYakymets <s28296@pjwstk.edu.pl>
2024-03-19 22:01:16 +0100

    task 9 square generation method is overridden in the CubicGenerator class

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from square_generator.square_generator import SquareGenerator
 
 
 class CubicGenerator(SquareGenerator):
+    def e_squares(self, start, end):
+        if start > end:
+            raise Exception("The end of the range cannot be less than the start.")
+
+        squares = [number * number for number in range(start, end)]
+        return squares
+
     def cubes(self, start, end):
         cubes = [number ** 3 for number in range(start, end)]
         return cubes
