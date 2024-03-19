00d71a2232a45e18418ba59c2e19d1dcff69dea2
SolomiiaYakymets <s28296@pjwstk.edu.pl>
2024-03-19 22:27:15 +0100

    task 10 SquareGenerator class is converted into an abstract base class

--- main.py
+++ main.py
@@ -18,5 +18,8 @@ class CubicGenerator(SquareGenerator):
 
 starting_num = int(input("Enter the starting number: "))
 ending_num = int(input("Enter the ending number: ")) + 1
-square_generator = SquareGenerator()
-square_root = [math.sqrt(number) for number in square_generator.e_squares(starting_num, ending_num)]
+cubic_generator = CubicGenerator()
+print(cubic_generator.e_squares(starting_num, ending_num))
+square_root = [math.sqrt(number) for number in cubic_generator.e_squares(starting_num, ending_num)]
+print(square_root)
+print(cubic_generator.cubes(starting_num, ending_num))
--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,4 +1,13 @@
-class SquareGenerator:
+from abc import ABC, abstractmethod
+
+
+class SquareGenerator(ABC):
+    @abstractmethod
+    def e_squares(self, start, end):
+        pass
+
+
+class MySquareGenerator(SquareGenerator):
     def e_squares(self, start, end):
         if start > end:
             raise Exception("The end of the range cannot be less than the start.")
