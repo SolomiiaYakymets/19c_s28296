829f34ab443ec72c35ba344c59907c386a398c42
SolomiiaYakymets <s28296@pjwstk.edu.pl>
2024-03-19 21:50:49 +0100

    task 8 subclass called CubicGenerator is created

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import math
 
 from square_generator.square_generator import SquareGenerator
 
+
+class CubicGenerator(SquareGenerator):
+    def cubes(self, start, end):
+        cubes = [number ** 3 for number in range(start, end)]
+        return cubes
+
+
 starting_num = int(input("Enter the starting number: "))
 ending_num = int(input("Enter the ending number: ")) + 1
 square_generator = SquareGenerator()
