091f64932b8aff496997a9297cadd5b5febd433a
SolomiiaYakymets <s28296@pjwstk.edu.pl>
2024-03-19 16:33:28 +0100

    task 3 class SquareGenerator is created

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# task2
-def e_squares(start, end):
-    squares = [number * number for number in range(start, end)]
-    return squares
+class SquareGenerator:
+    def e_squares(self, start, end):
+        squares = [number * number for number in range(start, end)]
+        return squares
